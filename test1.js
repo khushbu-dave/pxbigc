747aee01822d71cb3e71ee20e76853a6bedb2c09
khushbu-dave <143165033+khushbu-dave@users.noreply.github.com>
2024-06-28 18:12:09 +0530

    change

--- test1.js
+++ test1.js
@@ -1,6 +1,4 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <script type="text/javascript"> 
-   $(document).ready(function() {     
-     alert("document is ready");
-    });
+   console.log('HERE');
 </script>
